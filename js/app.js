7291f84a866bdc75aac8140203c76feee116dd44
YG777 <katyake@gamil.com>
2017-07-04 14:36:46 +0100

    w03d02 game logic works, ui to do , coach suggested refactoring logic and adding ui

--- js/app.js
+++ js/app.js
@@ -1,158 +1,129 @@
 document.addEventListener('DOMContentLoaded', function() {
   //CLASSWORK W03D01 PM
-  var tries = 0;
   var score = 0;
   var toFlip = 4;
-  var selectedCards = [];
-  var currentCountDown = createCountDown(30000);
-  var countDownValue = currentCountDown();
 
   cards = [{
-      name: "one",
-      id: 1,
-      matched: false,
-      selected: false
-    },
-    {
-      name: "one",
-      id: 2,
-      matched: false,
-      selected: false
-    },
-    {
-      name: "two",
-      id: 3,
-      matched: false,
-      selected: false
-    },
-    {
-      name: "two",
-      id: 4,
-      matched: false,
-      selected: false
-    },
-  ];
+    name: "one",
+    id: 1,
+    matched: false,
+    selected: false
+  },
+  {
+    name: "one",
+    id: 2,
+    matched: false,
+    selected: false
+  },
+  {
+    name: "two",
+    id: 3,
+    matched: false,
+    selected: false
+  },
+  {
+    name: "two",
+    id: 4,
+    matched: false,
+    selected: false
+  },
+];
 
-  function shuffle(a) {
-    for (let i = a.length; i; i--) {
-      let j = Math.floor(Math.random() * i);
-      [a[i - 1], a[j]] = [a[j], a[i - 1]];
-    }
+function shuffle(a) {
+  for (let i = a.length; i; i--) {
+    let j = Math.floor(Math.random() * i);
+    [a[i - 1], a[j]] = [a[j], a[i - 1]];
   }
-  shuffle(cards);
+}
+shuffle(cards);
 
-  function select() {
-    for (var i = 0; i < 2; i++) {
-      let cardSel = cards[i];
+
+//id comes from UI
+function select(id) {
+  for (var i = 0; i < cards.length; i++) {
+    let cardSel = cards[i];
+    if (id === cardSel.id) {
       cardSel.selected = true;
-      selectedCards.push(cardSel);
-      toFlip -= 1;
-      //UI ->flip the card;
+      toFlip -=1;
     }
   }
-  select(cards);
+}
+select(3);//testing
+select(2);
 
-  function match() {
-    if (selectedCards[0].name === selectedCards[1].name) {
-      score += 15;
-      toFlip -= 2;
-      console.log("match");
-    } else {
-      //UI ->flip back;
-      toFlip += 2;
-      console.log("no match")
+function match() {
+  var filtered = cards.filter(function(crd) {
+    if (crd.selected === true) {
+      crd.matched = true;
+      // console.table(cards);
+      return crd;
     }
-    if (toFlip === 0) {
-      //UI -> alert user "you won, want to play again?" or next level...
-    }
+  })
+
+  if (filtered[0].name === filtered[1].name) {
+    console.log("Win!");
+    score += 15;
+  } else {
+    //UI --> hide cards
+    console.log("no match");
+    toFlip += 2;
   }
-
-  match();
-  console.log(toFlip, score);
-
-  function createCountDown(timeRemaining) {
-    var startTime = Date.now();
-    return function() {
-      return timeRemaining - (Date.now() - startTime);
-    }
-  }
-//!!!check if works
-  function restart() {
-    if (countDownValue === 0){
-      //UI ->alert user "you lost"
-      //set the document.location.href attribute to the empty string (“”) try location.reload();
-    }
-  }
-
+}
+match(cards);
+console.log(score);
+console.log(toFlip);
+// console.table(cards);
 });
 
+$(init);
 
+function init() {
+  shuffle(array);
+  createCards();
+}
 
+function shuffle(a) {
+  for (let i = a.length; i; i--) {
+    let j = Math.floor(Math.random() * i);
+    [a[i - 1], a[j]] = [a[j], a[i - 1]];
+  }
+}
 
+function createCards() {
+  for (var i = 0; i < array.length; i++) {
+    $(`<li id=${array[i]}></li>`).appendTo($('.game-board'));
+  }
 
+  addClickEvents();
+}
 
+function addClickEvents() {
+  $('li').on('click', handleClick);
+}
 
+function handleClick() {
+  // on the first click, assign id from element to a vairable, also add class to selected element
+  // on the second click, assign id from element to a vairable, also add class to selected element
+  // firstValue & secondValue
 
+  // call a function to compare both values
+  console.log($(this).attr('id'));
+}
 
-//CLASSWORK W03D01 AM
-//   var score = 0;
-//   var cardsInGame = [];
-//   var matched = [];
-//   var toMatch = [];
-//
-//   cards = [
-//     {name: "one", id: 1, matched: false, selected: false},
-//     {name: "one", id: 2, matched: false, selected: false},
-//     {name: "two", id: 3, matched: false, selected: false},
-//     {name: "two", id: 4, matched: false, selected: false},
-//   ];
-// //shuffle the cards
-//   function shuffle(a) {
-//     for (let i = a.length; i; i--) {
-//       let j = Math.floor(Math.random() * i);
-//       [a[i - 1], a[j]] = [a[j], a[i - 1]];
-//     }
-//   }
-//   shuffle(cards);
-//   //push into gamecards array
-//   cardsInGame.push(cards);
-//
-//   //selected cards trigger on click -->
-//   //run selected function: change selected to true and push to tomatch arr
-//   for (var i = 0; i < 2; i++){
-//     var selectedCards = cardsInGame[i];
-//     //show selectedCards.name
-//     selectedCards.selected = true;
-//     selectedCards.push(toMatch);
-//    }
-//
-//   function matchCards() {
-//     for (var i = 0; i < 2; i++){
-//       var checkMatch = toMatch[i];
-//       if (checkMatch[0]===checkMatch[1]) {
-//         score += 1;
-//         checkMatch.matched = true;
-//         checkMatch.push(matched);
-//         //!!!!!!!!ADD SCORE!!!!!!
-//       }
-//       else {
-//         //hide the name
-//         checkMatch.push(cardsInGame);
-//       }
-//     }
-//     if cardsInGame.length = 0;
-//     alert("you won!");
-//   }
+function compareValues() {
+  // if both values are the same
+    // add to score and leave flip class on element
+    // firstValue & secondValue === ''
 
-// });
-// $(document).ready(function() {
-// console.log( "ready!" );
-// function createEls() {
-//   var heading = $('<h1></h1>').text("HEADING") ;
-//   var scoreDislay = $('<div></div>').text("div-Scoredisplay");
-//   var gameCont = $('<ul></ul>').text("Container");
-//
-//   $("body").append(heading, scoreDislay, gameCont, );
-// }
-// createEls();
-// });
+  // if values are different
+    // remove flip class from both elements
+    // firstValue & secondValue === ''
+
+  // checkForWin();
+}
+
+function checkForWin() {
+  // check if all lis have the class of flipped
+  // if true, Win
+  // if false, carry on with the game
+}
